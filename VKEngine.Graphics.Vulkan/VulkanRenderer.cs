cbda398b45b2e9e055eb518d75400c55385b8b94
KingoBoiii <kingodk@outlook.dk>
2024-04-29 20:50:45 +0200

    Added Queue families

--- VKEngine.Graphics.Vulkan/VulkanRenderer.cs
+++ VKEngine.Graphics.Vulkan/VulkanRenderer.cs
@@ -7,6 +7,13 @@ using static Vulkan.VulkanNative;
 
 namespace VKEngine.Graphics.Vulkan;
 
+internal struct QueueFamilyIndices
+{
+    public uint Graphics;
+    public uint Compute;
+    public uint Transfer;
+}
+
 internal sealed class VulkanRenderer : IRenderer
 {
     private VkInstance Instance { get; set; }
@@ -15,12 +22,19 @@ internal sealed class VulkanRenderer : IRenderer
     public VkPhysicalDeviceMemoryProperties DeviceMemoryProperties { get; set; }
     public VkPhysicalDeviceProperties DeviceProperties { get; set; }
     public VkPhysicalDeviceFeatures DeviceFeatures { get; set; }
+    public NativeList<VkQueueFamilyProperties> QueueFamilyProperties { get; } = new NativeList<VkQueueFamilyProperties>();
+    public QueueFamilyIndices QFIndices;
+    public VkCommandPool CommandPool { get; set; }
+
+    public VkPhysicalDeviceFeatures enabledFeatures { get; set; }
+    public NativeList<IntPtr> EnabledExtensions { get; } = new NativeList<IntPtr>();
 
     public void Initialize()
     {
         Instance = CreateInstance();
         PhysicalDevice = GetPhysicalDevice();
-        Device = CreateLogicalDevice();
+        Device = CreateLogicalDevice(enabledFeatures, EnabledExtensions);
+
     }
 
     public void Cleanup()
@@ -32,11 +46,185 @@ internal sealed class VulkanRenderer : IRenderer
     {
     }
 
-    private unsafe VkDevice CreateLogicalDevice()
+    private unsafe VkDevice CreateLogicalDevice(VkPhysicalDeviceFeatures enabledFeatures, NativeList<IntPtr> enabledExtensions, bool useSwapChain = true, VkQueueFlags requestedQueueTypes = VkQueueFlags.Graphics | VkQueueFlags.Compute)
     {
+        uint queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(PhysicalDevice, ref queueFamilyCount, null);
+        Debug.Assert(queueFamilyCount > 0);
+        QueueFamilyProperties.Resize(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(PhysicalDevice, &queueFamilyCount, (VkQueueFamilyProperties*)QueueFamilyProperties.Data.ToPointer());
+        QueueFamilyProperties.Count = queueFamilyCount;
 
+        // Get list of supported extensions
+        uint extCount = 0;
+        vkEnumerateDeviceExtensionProperties(PhysicalDevice, (byte*)null, ref extCount, null);
+        if (extCount > 0)
+        {
+            VkExtensionProperties* extensions = stackalloc VkExtensionProperties[(int)extCount];
+            if (vkEnumerateDeviceExtensionProperties(PhysicalDevice, (byte*)null, ref extCount, extensions) == VkResult.Success)
+            {
+                for (uint i = 0; i < extCount; i++)
+                {
+                    var ext = extensions[i];
+                    // supportedExtensions.push_back(ext.extensionName);
+                    // TODO: fixed-length char arrays are not being parsed correctly.
+                }
+            }
+        }
 
-        return VkDevice.Null;
+        // Desired queues need to be requested upon logical device creation
+        // Due to differing queue family configurations of Vulkan implementations this can be a bit tricky, especially if the application
+        // requests different queue types
+
+        using (NativeList<VkDeviceQueueCreateInfo> queueCreateInfos = new NativeList<VkDeviceQueueCreateInfo>())
+        {
+            float defaultQueuePriority = 0.0f;
+
+            // Graphics queue
+            if ((requestedQueueTypes & VkQueueFlags.Graphics) != 0)
+            {
+                QFIndices.Graphics = GetQueueFamilyIndex(VkQueueFlags.Graphics);
+                VkDeviceQueueCreateInfo queueInfo = new VkDeviceQueueCreateInfo();
+                queueInfo.sType = VkStructureType.DeviceQueueCreateInfo;
+                queueInfo.queueFamilyIndex = QFIndices.Graphics;
+                queueInfo.queueCount = 1;
+                queueInfo.pQueuePriorities = &defaultQueuePriority;
+                queueCreateInfos.Add(queueInfo);
+            }
+            else
+            {
+                QFIndices.Graphics = (uint)NullHandle;
+            }
+
+            // Dedicated compute queue
+            if ((requestedQueueTypes & VkQueueFlags.Compute) != 0)
+            {
+                QFIndices.Compute = GetQueueFamilyIndex(VkQueueFlags.Compute);
+                if (QFIndices.Compute != QFIndices.Graphics)
+                {
+                    // If compute family index differs, we need an additional queue create info for the compute queue
+                    VkDeviceQueueCreateInfo queueInfo = new VkDeviceQueueCreateInfo();
+                    queueInfo.sType = VkStructureType.DeviceQueueCreateInfo;
+                    queueInfo.queueFamilyIndex = QFIndices.Compute;
+                    queueInfo.queueCount = 1;
+                    queueInfo.pQueuePriorities = &defaultQueuePriority;
+                    queueCreateInfos.Add(queueInfo);
+                }
+            }
+            else
+            {
+                // Else we use the same queue
+                QFIndices.Compute = QFIndices.Graphics;
+            }
+
+            // Dedicated transfer queue
+            if ((requestedQueueTypes & VkQueueFlags.Transfer) != 0)
+            {
+                QFIndices.Transfer = GetQueueFamilyIndex(VkQueueFlags.Transfer);
+                if (QFIndices.Transfer != QFIndices.Graphics && QFIndices.Transfer != QFIndices.Compute)
+                {
+                    // If compute family index differs, we need an additional queue create info for the transfer queue
+                    VkDeviceQueueCreateInfo queueInfo = new VkDeviceQueueCreateInfo();
+                    queueInfo.sType = VkStructureType.DeviceQueueCreateInfo;
+                    queueInfo.queueFamilyIndex = QFIndices.Transfer;
+                    queueInfo.queueCount = 1;
+                    queueInfo.pQueuePriorities = &defaultQueuePriority;
+                    queueCreateInfos.Add(queueInfo);
+                }
+            }
+            else
+            {
+                // Else we use the same queue
+                QFIndices.Transfer = QFIndices.Graphics;
+            }
+
+            // Create the logical device representation
+            using (NativeList<IntPtr> deviceExtensions = new NativeList<IntPtr>(enabledExtensions))
+            {
+                if (useSwapChain)
+                {
+                    // If the device will be used for presenting to a display via a swapchain we need to request the swapchain extension
+                    deviceExtensions.Add(Strings.VK_KHR_SWAPCHAIN_EXTENSION_NAME);
+                }
+
+                VkDeviceCreateInfo deviceCreateInfo = VkDeviceCreateInfo.New();
+                deviceCreateInfo.queueCreateInfoCount = queueCreateInfos.Count;
+                deviceCreateInfo.pQueueCreateInfos = (VkDeviceQueueCreateInfo*)queueCreateInfos.Data.ToPointer();
+                deviceCreateInfo.pEnabledFeatures = &enabledFeatures;
+
+                if (deviceExtensions.Count > 0)
+                {
+                    deviceCreateInfo.enabledExtensionCount = deviceExtensions.Count;
+                    deviceCreateInfo.ppEnabledExtensionNames = (byte**)deviceExtensions.Data.ToPointer();
+                }
+
+                VkResult result = vkCreateDevice(PhysicalDevice, &deviceCreateInfo, null, out var device);
+                if (result != VkResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create Vulkan instance. Error: " + result);
+                }
+
+                CommandPool = CreateCommandPool(device, QFIndices.Graphics);
+
+                return device;
+            }
+        }
+    }
+
+    private uint GetQueueFamilyIndex(VkQueueFlags queueFlags)
+    {
+        // Dedicated queue for compute
+        // Try to find a queue family index that supports compute but not graphics
+        if ((queueFlags & VkQueueFlags.Compute) != 0)
+        {
+            for (uint i = 0; i < QueueFamilyProperties.Count; i++)
+            {
+                if (((QueueFamilyProperties[i].queueFlags & queueFlags) != 0)
+                    && (QueueFamilyProperties[i].queueFlags & VkQueueFlags.Graphics) == 0)
+                {
+                    return i;
+                }
+            }
+        }
+
+        // Dedicated queue for transfer
+        // Try to find a queue family index that supports transfer but not graphics and compute
+        if ((queueFlags & VkQueueFlags.Transfer) != 0)
+        {
+            for (uint i = 0; i < QueueFamilyProperties.Count; i++)
+            {
+                if (((QueueFamilyProperties[i].queueFlags & queueFlags) != 0)
+                    && (QueueFamilyProperties[i].queueFlags & VkQueueFlags.Graphics) == 0
+                    && (QueueFamilyProperties[i].queueFlags & VkQueueFlags.Compute) == 0)
+                {
+                    return i;
+                }
+            }
+        }
+
+        // For other queue types or if no separate compute queue is present, return the first one to support the requested flags
+        for (uint i = 0; i < QueueFamilyProperties.Count; i++)
+        {
+            if ((QueueFamilyProperties[i].queueFlags & queueFlags) != 0)
+            {
+                return i;
+            }
+        }
+
+        throw new InvalidOperationException("Could not find a matching queue family index");
+    }
+
+    private unsafe VkCommandPool CreateCommandPool(VkDevice device, uint queueFamilyIndex, VkCommandPoolCreateFlags createFlags = VkCommandPoolCreateFlags.ResetCommandBuffer)
+    {
+        VkCommandPoolCreateInfo cmdPoolInfo = VkCommandPoolCreateInfo.New();
+        cmdPoolInfo.queueFamilyIndex = queueFamilyIndex;
+        cmdPoolInfo.flags = createFlags;
+        var result = vkCreateCommandPool(device, &cmdPoolInfo, null, out VkCommandPool cmdPool);
+        if (result != VkResult.Success)
+        {
+            throw new InvalidOperationException("Could not create Vulkan instance. Error: " + result);
+        }
+        return cmdPool;
     }
 
     private unsafe VkPhysicalDevice GetPhysicalDevice()
@@ -70,16 +258,16 @@ internal sealed class VulkanRenderer : IRenderer
         // Store properties (including limits) and features of the phyiscal Device
         // So examples can check against them and see if a feature is actually supported
         VkPhysicalDeviceProperties deviceProperties;
-        vkGetPhysicalDeviceProperties(PhysicalDevice, &deviceProperties);
+        vkGetPhysicalDeviceProperties(physicalDevice, &deviceProperties);
         DeviceProperties = deviceProperties;
 
         VkPhysicalDeviceFeatures deviceFeatures;
-        vkGetPhysicalDeviceFeatures(PhysicalDevice, &deviceFeatures);
+        vkGetPhysicalDeviceFeatures(physicalDevice, &deviceFeatures);
         DeviceFeatures = deviceFeatures;
 
         // Gather physical Device memory properties
         VkPhysicalDeviceMemoryProperties deviceMemoryProperties;
-        vkGetPhysicalDeviceMemoryProperties(PhysicalDevice, &deviceMemoryProperties);
+        vkGetPhysicalDeviceMemoryProperties(physicalDevice, &deviceMemoryProperties);
         DeviceMemoryProperties = deviceMemoryProperties;
 
         return physicalDevice;
