1816c5d0859950e2a8f031fa651c36757c2deafb
KingoBoiii <kingodk@outlook.dk>
2024-05-10 12:40:46 +0200

    DescriptorSet now support Textures

--- VKEngine.Graphics.Vulkan/VulkanDescriptorSet.cs
+++ VKEngine.Graphics.Vulkan/VulkanDescriptorSet.cs
@@ -7,20 +7,25 @@ namespace VKEngine.Graphics.Vulkan;
 
 internal sealed class VulkanDescriptorSetFactory(IGraphicsConfiguration graphicsConfiguration, IVulkanLogicalDevice logicalDevice) : IDescriptorSetFactory
 {
-    public IDescriptorSet CreateDescriptorSet<T>(IBuffer buffer)
+    public IDescriptorSet CreateDescriptorSet<T>(IBuffer buffer, ITexture texture)
     {
-        return CreateDescriptorSet<T>(graphicsConfiguration.FramesInFlight, buffer);
+        return CreateDescriptorSet<T>(graphicsConfiguration.FramesInFlight, buffer, texture);
     }
 
-    public IDescriptorSet CreateDescriptorSet<T>(uint maxSets, IBuffer buffer)
+    public IDescriptorSet CreateDescriptorSet<T>(uint maxSets, IBuffer buffer, ITexture texture)
     {
         if (buffer is not VulkanBuffer vulkanBuffer)
         {
             throw new InvalidOperationException("Invalid buffer type!");
         }
 
+        if (texture is not VulkanTexture vulkanTexture)
+        {
+            throw new InvalidOperationException("Invalid texture type!");
+        }
+
         var descriptorSet = new VulkanDescriptorSet(graphicsConfiguration, logicalDevice);
-        descriptorSet.Initialize<T>(maxSets, vulkanBuffer);
+        descriptorSet.Initialize<T>(maxSets, vulkanBuffer, vulkanTexture);
         return descriptorSet;
     }
 }
@@ -31,22 +36,44 @@ internal sealed class VulkanDescriptorSet(IGraphicsConfiguration graphicsConfigu
     internal VkDescriptorSetLayout descriptorSetLayout = VkDescriptorSetLayout.Null;
     internal VkDescriptorSet[] descriptorSets = [];
 
-    internal unsafe void Initialize<T>(uint maxSets, VulkanBuffer vulkanBuffer)
+    internal unsafe void Initialize<T>(uint maxSets, VulkanBuffer vulkanBuffer, VulkanTexture vulkanTexture)
     {
         CreateDescriptorPool(maxSets);
 
-        var descriptorSetLayoutBinding = new VkDescriptorSetLayoutBinding
-        {
-            binding = 0,
-            descriptorType = VkDescriptorType.UniformBuffer,
-            descriptorCount = 1,
-            stageFlags = VkShaderStageFlags.Vertex,
-            pImmutableSamplers = null
+        var descriptorSetLayoutBindings = new VkDescriptorSetLayoutBinding[] {
+            new() {
+                binding = 0,
+                descriptorType = VkDescriptorType.UniformBuffer,
+                descriptorCount = 1,
+                stageFlags = VkShaderStageFlags.Vertex,
+                pImmutableSamplers = null
+            },
+            new() {
+                binding = 1,
+                descriptorType = VkDescriptorType.CombinedImageSampler,
+                descriptorCount = 1,
+                stageFlags = VkShaderStageFlags.Fragment,
+                pImmutableSamplers = null
+            }
         };
 
+        //var descriptorSetLayoutBinding = new VkDescriptorSetLayoutBinding
+        //{
+        //    binding = 0,
+        //    descriptorType = VkDescriptorType.UniformBuffer,
+        //    descriptorCount = 1,
+        //    stageFlags = VkShaderStageFlags.Vertex,
+        //    pImmutableSamplers = null
+        //};
+
         var descriptorSetLayoutCreateInfo = VkDescriptorSetLayoutCreateInfo.New();
-        descriptorSetLayoutCreateInfo.bindingCount = 1;
-        descriptorSetLayoutCreateInfo.pBindings = &descriptorSetLayoutBinding;
+        fixed (VkDescriptorSetLayoutBinding* pDescriptorSetLayoutBindings = &descriptorSetLayoutBindings[0])
+        {
+            descriptorSetLayoutCreateInfo.bindingCount = (uint)descriptorSetLayoutBindings.Length;
+            descriptorSetLayoutCreateInfo.pBindings = pDescriptorSetLayoutBindings;
+        }
+        //descriptorSetLayoutCreateInfo.bindingCount = 1;
+        //descriptorSetLayoutCreateInfo.pBindings = &descriptorSetLayoutBinding;
 
         if (vkCreateDescriptorSetLayout(logicalDevice.Device, &descriptorSetLayoutCreateInfo, null, out descriptorSetLayout) is not VkResult.Success)
         {
@@ -80,15 +107,50 @@ internal sealed class VulkanDescriptorSet(IGraphicsConfiguration graphicsConfigu
                 range = (ulong)Unsafe.SizeOf<T>()
             };
 
-            VkWriteDescriptorSet writeDescriptorSet = VkWriteDescriptorSet.New();
-            writeDescriptorSet.dstSet = descriptorSets[i];
-            writeDescriptorSet.dstBinding = 0;
-            writeDescriptorSet.dstArrayElement = 0;
-            writeDescriptorSet.descriptorType = VkDescriptorType.UniformBuffer;
-            writeDescriptorSet.descriptorCount = 1;
-            writeDescriptorSet.pBufferInfo = &descriptorBufferInfo;
+            var descriptorImageInfo = new VkDescriptorImageInfo
+            {
+                sampler = vulkanTexture.sampler,
+                imageView = ((VulkanImage)vulkanTexture.Image).imageView,
+                imageLayout = VkImageLayout.ShaderReadOnlyOptimal
+            };
 
-            vkUpdateDescriptorSets(logicalDevice.Device, 1, &writeDescriptorSet, 0, null);
+            var writeDescriptorSets = new VkWriteDescriptorSet[]
+            {
+                new()
+                {
+                    sType = VkStructureType.WriteDescriptorSet,
+                    dstSet = descriptorSets[i],
+                    dstBinding = 0,
+                    dstArrayElement = 0,
+                    descriptorType = VkDescriptorType.UniformBuffer,
+                    descriptorCount = 1,
+                    pBufferInfo = &descriptorBufferInfo
+                },
+                new()
+                {
+                    sType = VkStructureType.WriteDescriptorSet,
+                    dstSet = descriptorSets[i],
+                    dstBinding = 1,
+                    dstArrayElement = 0,
+                    descriptorType = VkDescriptorType.CombinedImageSampler,
+                    descriptorCount = 1,
+                    pImageInfo = &descriptorImageInfo
+                }
+            };
+
+            //VkWriteDescriptorSet writeDescriptorSet = VkWriteDescriptorSet.New();
+            //writeDescriptorSet.dstSet = descriptorSets[i];
+            //writeDescriptorSet.dstBinding = 0;
+            //writeDescriptorSet.dstArrayElement = 0;
+            //writeDescriptorSet.descriptorType = VkDescriptorType.UniformBuffer;
+            //writeDescriptorSet.descriptorCount = 1;
+            //writeDescriptorSet.pBufferInfo = &descriptorBufferInfo;
+
+            fixed(VkWriteDescriptorSet* writeDescriptorSet = &writeDescriptorSets[0])
+            {
+                vkUpdateDescriptorSets(logicalDevice.Device, (uint)writeDescriptorSets.Length, writeDescriptorSet, 0, null);
+            }
+            //vkUpdateDescriptorSets(logicalDevice.Device, writeDescriptorSets.Length, &writeDescriptorSet[0], 0, null);
         }
     }
 
@@ -100,19 +162,31 @@ internal sealed class VulkanDescriptorSet(IGraphicsConfiguration graphicsConfigu
 
     private unsafe void CreateDescriptorPool(uint maxSets)
     {
-        VkDescriptorPoolSize descriptorPoolSize = new()
+        var descriptorPoolSize = new VkDescriptorPoolSize[]
         {
-            type = VkDescriptorType.UniformBuffer,
-            descriptorCount = 1
+            new() {
+                type = VkDescriptorType.UniformBuffer,
+                descriptorCount = graphicsConfiguration.FramesInFlight
+            },
+            new() {
+                type = VkDescriptorType.CombinedImageSampler,
+                descriptorCount = graphicsConfiguration.FramesInFlight
+            }
         };
 
-        VkDescriptorPoolCreateInfo descriptorPoolCreateInfo = new()
+        //VkDescriptorPoolSize descriptorPoolSize = new()
+        //{
+        //    type = VkDescriptorType.UniformBuffer,
+        //    descriptorCount = 1
+        //};
+
+        var descriptorPoolCreateInfo = VkDescriptorPoolCreateInfo.New();
+        descriptorPoolCreateInfo.maxSets = maxSets;
+        fixed (VkDescriptorPoolSize* pDescriptorPoolSize = &descriptorPoolSize[0])
         {
-            sType = VkStructureType.DescriptorPoolCreateInfo,
-            maxSets = maxSets,
-            poolSizeCount = 1,
-            pPoolSizes = &descriptorPoolSize
-        };
+            descriptorPoolCreateInfo.poolSizeCount = (uint)descriptorPoolSize.Length;
+            descriptorPoolCreateInfo.pPoolSizes = pDescriptorPoolSize;
+        }
 
         if (vkCreateDescriptorPool(logicalDevice.Device, &descriptorPoolCreateInfo, null, out descriptorPool) is not VkResult.Success)
         {
