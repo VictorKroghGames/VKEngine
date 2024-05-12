6e2e45a153099a3d10255164b8775ec7715a41dd
KingoBoiii <kingodk@outlook.dk>
2024-05-12 18:39:56 +0200

    Removed old and out-commented code

--- VKEngine.Graphics.Abstractions/IPipeline.cs
+++ VKEngine.Graphics.Abstractions/IPipeline.cs
@@ -2,39 +2,9 @@
 
 namespace VKEngine.Graphics;
 
-
-public readonly struct PipelineLayoutVertexAttribute(uint binding, uint location, Format format, uint offset)
-{
-    public readonly uint binding = binding;
-    public readonly uint location = location;
-    public readonly Format format = format;
-    public readonly uint offset = offset;
-}
-
-public readonly struct PipelineLayout(uint binding, uint stride, VertexInputRate inputRate, params PipelineLayoutVertexAttribute[] vertexAttributes)
-{
-    public readonly uint binding = binding;
-    public readonly uint stride = stride;
-    public readonly VertexInputRate inputRate = inputRate;
-    public readonly PipelineLayoutVertexAttribute[] vertexAttributes = vertexAttributes;
-}
-
-public readonly struct PipelineSpecification
-{
-    public FrontFace FrontFace { get; init; }
-    public CullMode CullMode { get; init; }
-    public PipelineLayout PipelineLayout { get; init; }
-
-    public IShader Shader { get; init; }
-    public IRenderPass RenderPass { get; init; }
-}
-
 public interface IPipelineFactory
 {
     IPipeline CreateGraphicsPipeline(PipelineDescription pipelineDescription);
-
-    [Obsolete(error: true, message: "Use CreateGraphicsPipeline(PipelineDescription) instead.")]
-    IPipeline CreateGraphicsPipeline(PipelineSpecification specification, params IDescriptorSet[] descriptorSets);
 }
 
 public interface IPipeline
