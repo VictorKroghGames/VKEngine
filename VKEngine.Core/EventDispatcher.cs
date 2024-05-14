14d7b3f0df9dbc2db1e450e1f175a955cdee91b8
KingoBoiii <kingodk@outlook.dk>
2024-05-14 22:02:22 +0200

    Added EventDispatcher implementation. And register it into IoC container

---
+++ VKEngine.Core/EventDispatcher.cs
@@ -0,0 +1,20 @@
+﻿namespace VKEngine.Core;
+
+internal sealed class EventDispatcher : IEventDispatcher
+{
+    public bool Dispatch<TEvent>(IEvent e, Func<TEvent, bool> func) where TEvent : IEvent
+    {
+        if (e is null)
+        {
+            return false;
+        }
+
+        if (e is TEvent @event)
+        {
+            e.Handled |= func(@event);
+            return e.Handled;
+        }
+
+        return false;
+    }
+}
