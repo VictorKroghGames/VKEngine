dbfe5f0f7c23a52738ce12b21146147b5f449878
KingoBoiii <kingodk@outlook.dk>
2024-05-17 06:26:04 +0200

    Added DesciptorSetDescription

---
+++ VKEngine.Graphics.Abstractions/Data/DescriptorSetDescription.cs
@@ -0,0 +1,14 @@
+﻿namespace VKEngine.Graphics;
+
+public readonly struct DescriptorBinding
+{
+    public readonly uint Binding { get; init; }
+    public readonly DescriptorType DescriptorType { get; init; }
+    public readonly uint DescriptorCount { get; init; }
+    public readonly ShaderModuleType ShaderStageFlags { get; init; }
+}
+
+public readonly struct DescriptorSetDescription
+{
+    public readonly DescriptorBinding[] DescriptorBindings { get; init; }
+}
